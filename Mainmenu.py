1ce72d8d86cb5044273453fca6ba99e4279b9d7e
Adrian Munoz <munoza204@gmail.com>
2025-02-25 23:22:45 -0600

    added basic button class

--- Mainmenu.py
+++ Mainmenu.py
@@ -1,5 +1,7 @@
 import pygame
 import os
+import button
+
 # Initialize Pygame
 pygame.init()
 #get screen information
@@ -16,14 +18,21 @@ pygame.display.set_caption("Pygame Window")
 # big_font = pygame.font.Font('freesansbold.ttf', 50)
 #game vars and img
 
+# defining a button
+button = button.Button(screen, 'red', 50, 50, 100, 50)
+
 # Main game loop
 run = True
 while run:
     #draw a rectangle thats at pos 200 200 and make it 100 by 100 big
     pygame.draw.rect(screen, 'green', [screen_width/2-50,screen_height/2-50,100,100])
+    button.render()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if button.is_clicked(event):
+            print("FNSKAJDGBSJKD")
 
     # Update the display
     pygame.display.flip()
--- button.py
+++ button.py
@@ -1,2 +1,26 @@
 # button.py
 # potential subclass: Piece 
+import pygame
+
+class Button:
+
+    def __init__(self, surface, color, x, y, width, height):
+        self.surface = surface
+        self.color = color
+        self.x, self.y = x, y
+        self.width, self.height = width, height
+        self.rect = pygame.Rect(x, y, width, height)
+
+    def render(self):
+        pygame.draw.rect(self.surface, self.color, self.rect)
+
+    def is_left_click(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            return True
+        return False
+
+    def is_clicked(self, event):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mouse_pos) and self.is_left_click(event):
+            return True
+        return False
