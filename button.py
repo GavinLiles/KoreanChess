8354d386847563fa49dd6cad4a2dfaaca013e27e
Adrian Munoz <munoza204@gmail.com>
2025-02-26 07:17:09 -0600

    added texture button class

--- button.py
+++ button.py
@@ -4,7 +4,7 @@ import pygame
 
 class Button:
 
-    def __init__(self, surface, color, x, y, width, height):
+    def __init__(self, surface, x, y, width, height, color='white'):
         self.surface = surface
         self.color = color
         self.x, self.y = x, y
@@ -27,8 +27,8 @@ class Button:
     
 class TextButton(Button):
     
-    def __init__(self, surface, color, text, x, y, width, height):
-        super().__init__(surface, color, x, y, width, height)
+    def __init__(self, surface, x, y, width, height, color, text=""):
+        super().__init__(surface, x, y, width, height, color)
         
         # creating the text to render on button
         pygame.font.init()
@@ -38,6 +38,17 @@ class TextButton(Button):
     def render(self):
         pygame.draw.rect(self.surface, self.color, self.rect)
         self.surface.blit(self.text_surface, (self.x, self.y))
+
+class TextureButton(Button):
+    pass
+    def __init__(self, surface, x, y, width, height, image_file):
+        super().__init__(surface, x, y, width, height, color='white')
+        self.image = pygame.image.load(image_file)
+        self.image = pygame.transform.scale(self.image, (width, height))
+
+    def render(self):
+        super().render()
+        self.surface.blit(self.image, (self.x, self.y))
 
 
 if __name__ == "__main__":
@@ -55,8 +66,9 @@ if __name__ == "__main__":
     screen = pygame.display.set_mode((fullscreen_width - 10, fullscreen_height - 50), pygame.RESIZABLE)  # width x height
     pygame.display.set_caption("Pygame Window")
 
-    button1 = Button(screen, 'red', 50, 50, 50, 50)
-    button2 = TextButton(screen, 'green', "hello", 50, 100, 100, 50)
+    button1 = Button(screen, 50, 50, 50, 50, color='red')
+    button2 = TextButton(screen, 50, 100, 100, 50, color='green', text="hello")
+    button3 = TextureButton(screen, 50, 150, 100, 110, image_file="assets\dog.JPG")
 
     run = True
     background = pygame.image.load('assets/Janggi_Board.png').convert()
@@ -68,10 +80,18 @@ if __name__ == "__main__":
         
         button1.render()
         button2.render()
+        button3.render()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+            if button1.is_clicked(event):
+                print("button 1 pressed")
+            if button2.is_clicked(event):
+                print("button 2 pressed")
+            if button3.is_clicked(event):
+                print("button3 pressed")
 
         # Update the display
         pygame.display.update()
