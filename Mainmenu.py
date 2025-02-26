41d30e4c4c019ca6570932e51bff21af5cd17384
Gavin Liles <gavinliles@gmail.com>
2025-02-25 23:34:50 -0600

    main

--- Mainmenu.py
+++ Mainmenu.py
@@ -1,41 +1,38 @@
 import pygame
 import os
-import button
-
 # Initialize Pygame
 pygame.init()
 #get screen information
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 info = pygame.display.Info()
-screen_width, screen_height = info.current_w, info.current_h
+fullscreen_width, fullscreen_height = info.current_w, info.current_h
 
 
 # Set up the display window and make is resizeable
-screen = pygame.display.set_mode((screen_width - 10, screen_height - 50), pygame.RESIZABLE)  # width x height
+screen = pygame.display.set_mode((fullscreen_width - 10, fullscreen_height - 50), pygame.RESIZABLE)  # width x height
 pygame.display.set_caption("Pygame Window")
+
 #set the font
 # font = pygame.font.Font('freesansbold.ttf', 20)
 # big_font = pygame.font.Font('freesansbold.ttf', 50)
 #game vars and img
 
-# defining a button
-button = button.Button(screen, 'red', 50, 50, 100, 50)
+#pygame.draw.rect(screen, 'green', [screen_width/2-50,screen_height/2-50,100,100])
+# Main menu loop
+def main_menu():
+    run = True
+    background = pygame.image.load('clote0.bmp').convert()
+    while run:
+        screen_width, screen_height = screen.get_size()
+        screen.blit(background, (0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-# Main game loop
-run = True
-while run:
-    #draw a rectangle thats at pos 200 200 and make it 100 by 100 big
-    pygame.draw.rect(screen, 'green', [screen_width/2-50,screen_height/2-50,100,100])
-    button.render()
+        # Update the display
+        pygame.display.flip()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if button.is_clicked(event):
-            print("FNSKAJDGBSJKD")
+    # Quit Pygame
+    pygame.quit()
 
-    # Update the display
-    pygame.display.flip()
-
-# Quit Pygame
-pygame.quit()
+main_menu()
