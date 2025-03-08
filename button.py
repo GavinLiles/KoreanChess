8ca51ad806f40325a9529ce66aca93e4cd900cc8
Ilegal_Beagle <munoza204@gmail.com>
2025-03-08 14:51:50 -0600

    made piece classes

--- button.py
+++ button.py
@@ -9,9 +9,9 @@ class Button:
         self.hover_color = hover_color
         self.normal_color = color
         self.color = color
-        self.x, self.y = pos
+        self.pos = pos
         self.width, self.height = size
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.rect = pygame.Rect(self.pos[0], self.pos[1], self.width, self.height)
         self.func = func
 
     def render(self):
@@ -38,18 +38,18 @@ class Button:
         else:
             self.color = self.normal_color
         if self.is_clicked(event, mouse_pos):
-            self.func
+            self.func()
 
 
 class TextButton(Button):
     
-    def __init__(self, surface, pos, size, text="", func=None, color='white', hover_color='grey'):
+    def __init__(self, surface, pos, size, text="", func=None, color='white', hover_color='grey', font_size=30):
         super().__init__(surface, pos, size, func, color, hover_color)
         pygame.font.init()
-        self.font = pygame.font.SysFont('Comic Sans MS', 30)
+        self.font = pygame.font.SysFont('Comic Sans MS', font_size)
         self.text_surface = self.font.render(text, False, 'black')
-        self.text_rect = self.text_surface.get_rect(center=(self.x, self.y))
-    
+        self.text_rect = self.text_surface.get_rect(center=(self.pos[0] + self.width/2, self.pos[1] + self.height/2))
+
     def render(self):
         super().render()
         self.surface.blit(self.text_surface, self.text_rect)
@@ -58,12 +58,18 @@ class TextureButton(Button):
 
     def __init__(self, surface, pos, size, image_file, func, color='white', hover_color='grey'):
         super().__init__(surface, pos, size, func, color, hover_color)
-        self.image = pygame.image.load(image_file)
-        self.image = pygame.transform.scale(self.image, size)
-
+        self.set_image(image_file)
+        
+    def set_image(self, image_file):
+        try:
+            self.image = pygame.image.load(image_file).convert_alpha()
+            self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        except OSError:
+            print(f'file {image_file} not found. Exiting')
+            exit(0)
+            
     def render(self):
-        super().render()
-        self.surface.blit(self.image, (self.x, self.y))
+        self.surface.blit(self.image, self.pos)
 
 
 def button_func():
@@ -78,11 +84,11 @@ if __name__ == "__main__":
     #get screen information
     os.environ['SDL_VIDEO_CENTERED'] = '1'
     info = pygame.display.Info()
-    screen = pygame.display.set_mode((info.current_w - 10, info.current_h - 50), pygame.RESIZABLE)  # width x height
+    screen = pygame.display.set_mode((500, 500), pygame.RESIZABLE)  # width x height
 
-    button1 = Button(screen, [50, 50], [50, 50], func=button_func())
-    button2 = TextButton(screen, [50, 100], [100, 50], color='green', text="hello", func=button_func())
-    button3 = TextureButton(screen, [50, 150], [100, 110], image_file="assets\dog.JPG", func=button_func())
+    button1 = Button(screen, [50, 50], [50, 50], func=button_func)
+    button2 = TextButton(screen, [100, 250], [100, 100], color='green', text="hello", func=button_func)
+    button3 = TextureButton(screen, [50, 150], [100, 110], image_file="assets/dog.JPG", func=button_func)
 
     run = True
     background = pygame.image.load('assets/Janggi_Board.png').convert()
--- piece.py
+++ piece.py
@@ -1,23 +1,95 @@
 # piece.py
-# contains the definition of the piece class
 from button import *
 
 class Piece(TextureButton):
-    def __init__(self, surface, x, y, width, height, image_file):
-        super().__init__(surface, x, y, width, height, image_file)
+    def __init__(self, surface, pos, size, func, team=None, image_file='assets/Pieces/Blank_Piece.png'):
+        super().__init__(surface, pos, size, image_file, func)
         self.value = 0       # the value the piece is worth
         self.team = None     # team of the piece
         self.location = None # where the piece is located on the board
-
-    def get_team(self):
-        return self.team
-    
-    def get_location(self):
-        return self.location
-    
-    def get_value(self):
-        return self.value
     
 class Pawn(Piece):
-    def __init__(self, surface, x, y, width, height, image_file):
-        super().__init__(surface, x, y, width, height, image_file)
+    def __init__(self, surface, pos, size, func, team=None): 
+        super().__init__(surface, pos, size, image_file='assets/Pieces/Cho_Pawn.png', func=func)
+        self.value = 2
+
+    def get_possible_moves():
+        pass
+
+class Advisor(Piece):
+    def __init__(self, surface, pos, size, func, team=None): 
+        super().__init__(surface, pos, size, image_file='assets/Pieces/Cho_Advisor.png', func=func)
+        self.value = 3
+
+    def get_possible_moves():
+        pass
+
+class Elephant(Piece):
+    def __init__(self, surface, pos, size, func, team=None): 
+        super().__init__(surface, pos, size, image_file='assets/Pieces/Cho_Elepahnt.png', func=func)
+        self.value = 3
+
+    def get_possible_moves():
+        pass
+
+class Horse(Piece):
+    def __init__(self, surface, pos, size, func, team=None): 
+        super().__init__(surface, pos, size, image_file='assets/Pieces/Cho_Horse.png', func=func)
+        self.value = 5
+
+    def get_possible_moves():
+        pass
+
+class Cannon(Piece):
+    def __init__(self, surface, pos, size, func, team=None): 
+        super().__init__(surface, pos, size, image_file='assets/Pieces/Cho_Cannon.png', func=func)
+        self.value = 7
+
+    def get_possible_moves():
+        pass
+
+class Chariot(Piece):
+    def __init__(self, surface, pos, size, func, team=None): 
+        super().__init__(surface, pos, size, image_file='assets/Pieces/Cho_Chariot.png', func=func)
+        self.value = 13
+
+    def get_possible_moves():
+        pass
+
+if __name__ == '__main__':
+    import pygame
+    import os
+
+    pygame.init()
+    info = pygame.display.Info()
+    screen = pygame.display.set_mode((500, 500), pygame.RESIZABLE)
+    background = pygame.image.load('assets/Janggi_Board.png').convert()
+    run = True
+    def fun():
+        print("click")
+        
+    pieces = [ 
+        Chariot(screen, [50, 100], [50, 50], func=fun, team='cho'),
+        Pawn(screen, [100, 100], [50, 50], func=fun, team='cho'),
+        Cannon(screen, [150, 100], [50, 50], func=fun, team='cho')
+    ]
+
+    while run:
+        background = pygame.transform.smoothscale(background, screen.get_size())
+        screen.blit(background, (0, 0))
+        mouse_pos = pygame.mouse.get_pos()
+
+        for piece in pieces:
+            piece.render()
+
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            for piece in pieces:
+                piece.process(event, mouse_pos)
+
+        pygame.display.update()
+
+    # Quit Pygame
+    pygame.quit()
