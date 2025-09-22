b073ea2dc5fd6478d8d8b9b610c179e5093a25c4
Ilegal-Beagle <munoza204@gmail.com>
2025-09-22 10:52:47 -0500

    basic text class

--- text.py
+++ text.py
@@ -2,14 +2,22 @@
 import pygame
 
 class Text:
-    def __init__(self, text:str, font='comic sans ms', size=50, color=(0,0,0), antialias=True):
-        self.size, self.color, self.antialias = size, color, antialias
-        self.font = pygame.font.SysFont(font, size)
-        self.text = self.font.render(text, antialias, color)
-        self.size = self.font.size(text)
+    def __init__(self, pos=(0,0), text:str='', font='comic sans ms', size=50, color=(0,0,0), antialias=True):
+        self._aa = antialias
+        self._pos = pos
+        self._text = text
+        self._color = color
+        self._font = pygame.font.SysFont(font, size)
+        self._text_surface = self._font.render(text, antialias, color)
+        self.rect = self._text_surface.get_rect()
 
-    def render(self, surface, position:tuple):
-        surface.blit(self.text, position)
+    def render(self, surface):
+        surface.blit(self._text_surface, self._pos)
 
-    def change_text(self, text):
-        self.text = self.font.render(text, True, self.antialias, self.color)
+    def set_text(self, text:str):
+        self._text = text
+        self._text_surface = self._font.render(self._text, self._aa, self._color)
+        self.rect = self._text_surface.get_rect()
+
+    def set_pos(self, pos:tuple[int,int]):
+        self._pos = pos
